c7e2de64b0485e70fc78ceeb4a68b1e0abb5520c
Henrique <henrique.empresa53@outlook.com>
2021-11-29 14:54:14 -0300

    Adicionando projeto ao repositorio

---
+++ src/main/java/com/digitalhouse/testeInfra2/TesteInfra2Application.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.testeInfra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteInfra2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteInfra2Application.class, args);
+	}
+
+}
